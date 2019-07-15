3f0c4a6adb51ac61378b694eaf91d5a9a677e09f
Jan Ranostaj <HonzaRanostaj@gmail.com>
2019-07-15 22:46:10 +0200

    define and access the database

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -85,8 +85,10 @@ def create_app(test_config=None):
     def hello():
         return 'Hello, World!'
 
-    return app
+    from . import db
+    db.init_app(app)
 
+    return app
 
 # TO RUN THE APP:
 """
